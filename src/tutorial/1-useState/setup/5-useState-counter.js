903fe3e2c6720c9dd12adb581610f3c53fd158ac
thanhha-loz <nguyenthanhha.tai.555@gmail.com>
2020-12-20 21:54:51 +0700

    Update 5-useState-counter.js

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,12 +2,23 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
+  const complexCount = () => {
+    setTimeout(() => {
+      setValue((prevState) => {
+        return prevState +1;
+      })
+    },2000)
 
+  }
   return <>
   <h2>{value}</h2>
   <button className='btn' onClick={() => setValue(value +1)}>Increase</button>
     <button className='btn' onClick={() => setValue(0)}>Reset</button>
     <button className='btn' onClick={() => setValue(value - 1)}>Decrease</button>
+    <h1>Complex Counter</h1>
+    <h2>{value}</h2>
+    <button className='btn' onClick={complexCount}>Increase</button>
+
   </>
 };
 
